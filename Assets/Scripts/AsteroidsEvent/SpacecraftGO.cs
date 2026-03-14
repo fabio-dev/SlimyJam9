32836078723cb6dce40246ea654fc3d4255aad18
Fabio Godinho <fabio.godinho92@outlook.com>
2026-03-14 22:33:46 +0100

    Image vaisseau

--- Assets/Scripts/AsteroidsEvent/SpacecraftGO.cs
+++ Assets/Scripts/AsteroidsEvent/SpacecraftGO.cs
@@ -1,15 +1,43 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SpacecraftGO : MonoBehaviour
 {
     [SerializeField] private ShootController _shootController;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite[] _sprites;
 
     private Spacecraft _spacecraft;
+    private Sequence _animation;
+    private int _currentSpriteIndex;
 
     public void Bind(Spacecraft spacecraft)
     {
         _spacecraft = spacecraft;
         _shootController.Bind(spacecraft);
+
+        _animation = DOTween.Sequence();
+        _animation.SetLoops(-1);
+
+        foreach (var sprite in _sprites)
+        {
+            _animation.AppendCallback(SetNextSprite);
+            _animation.AppendInterval(.1f);
+        }
+
+        _animation.Play();
+    }
+
+    private void SetNextSprite()
+    {
+        _currentSpriteIndex++;
+
+        if (_currentSpriteIndex >= _sprites.Length)
+        {
+            _currentSpriteIndex = 0;
+        }
+
+        _spriteRenderer.sprite = _sprites[_currentSpriteIndex];
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,5 +51,10 @@ public class SpacecraftGO : MonoBehaviour
 
         _spacecraft.Damage(asteroid.Damage);
         asteroid.Destroy();
+    }
+
+    private void OnDestroy()
+    {
+        _animation.Kill();
     }
 }
